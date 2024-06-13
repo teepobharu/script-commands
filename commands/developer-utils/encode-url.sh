9de4994c7c83f554e728464b7ecce7f96672d83e
Teepob Harutaipree <Teepob.Harutaipree@agoda.com>
2024-06-13 15:59:30 +0700

    encode url using node

--- commands/developer-utils/encode-url.sh
+++ commands/developer-utils/encode-url.sh
@@ -12,5 +12,8 @@
 # Documentation:
 # @raycast.description Encodes clipboard content url and copies it again.
 
-pbpaste | ( curl -Gso /dev/null -w %{url_effective} --data-urlencode @- "" | cut -c 3- || true) | pbcopy
+# pbpaste | ( curl -Gso /dev/null -w %{url_effective} --data-urlencode @- "" | cut -c 3- || true) | pbcopy
+# echo "Encoded URL"
+
+pbpaste | node -e "const { URLSearchParams } = require('url'); const input = require('fs').readFileSync('/dev/stdin', 'utf-8').trim(); console.log(encodeURIComponent(input));" | pbcopy
 echo "Encoded URL"
